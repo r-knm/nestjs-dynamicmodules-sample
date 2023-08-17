9e9507aba7e5635fc63ff1e6e63a110dd28d71fe
r-knm <kainuma@log-build.com>
2023-08-17 19:19:53 +0900

    Implement DynamicService.

--- src/dynamic-feature/dynamic-feature.service.ts
+++ src/dynamic-feature/dynamic-feature.service.ts
@@ -1,10 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { SRCH_OPTIONS } from 'src/constants';
+import { CONFIG_OPTIONS } from 'src/constants';
+import { ConfigOptions } from 'src/interfaces';
+
+interface IDynamicService {
+  connect(): Promise<any>;
+}
 
 @Injectable()
-export class DynamicFeatureService<T> {
-  constructor(
-    @Inject(SRCH_OPTIONS)
-    private options: {},
-  ) {}
+export class DynamicFeatureService implements IDynamicService {
+  private myClient: any;
+
+  constructor(@Inject(CONFIG_OPTIONS) private options: ConfigOptions) {}
+
+  async connect(): Promise<any> {
+    return this.myClient;
+  }
 }
